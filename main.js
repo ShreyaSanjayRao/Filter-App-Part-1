88e40305d5644214c242789e8e18e3d28ae29ba1
ShreyaSanjayRao <73601962+ShreyaSanjayRao@users.noreply.github.com>
2021-06-17 20:49:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload()
+{
+
+}
+function setup()
+{
+ canvas=createCanvas(410,410);
+ canvas.center();
+}
+function draw()
+{
+
+}
+function pics()
+{
+    save("my_clown_images.png");
+}
